e44094d7372e452210feeb4297ff9f447928e168
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-05-16 14:53:50 +0900

    feat: project build

---
+++ src/main/java/com/example/sprbootdockerdemo/SprbootdockerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprbootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbootdockerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprbootdockerdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprbootdockerdemo/SprbootdockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprbootdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprbootdockerdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
